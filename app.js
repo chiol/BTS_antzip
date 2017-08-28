3cdedfa0dbada13c4251272c43889dd3a1b8c3b4
chiol <jcy6677@gmail.com>
2017-08-28 13:12:14 +0900

    css추가

--- app.js
+++ app.js
@@ -11,14 +11,20 @@ function search(name,phone) {
   for(names in ws){
     if(name == ws[names].v){
       if(phone == ws['C'+names.slice(1)].v){
-        return true;
+        return {
+          name:ws['A'+names.slice(1)].v,
+          address:ws['B'+names.slice(1)].v,
+          phone:ws['C'+names.slice(1)].v,
+          price:ws['D'+names.slice(1)].v,
+          count:ws['E'+names.slice(1)].v
+        };
       }
     }
   }
   return false;
 }
 //----
-
+app.use(express.static('public'));
 //template
 app.set('views','./views');
 app.set('view engine','jade');
