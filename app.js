a844567a3e51527297ea76fc5d17594f93865279
chiol <jcy6677@gmail.com>
2017-08-28 19:07:42 +0900

    version1.1

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var app = express();
-
-app.locals.pretty = true;
-
 //xlsx
 if(typeof require !== 'undefined') XLSX = require("xlsx");
-const wb = XLSX.readFile('test.xlsx');
+const wb = XLSX.readFile('list.xlsx');
 var ws = wb.Sheets.Sheet1;
 function search(name,phone) {
   for(names in ws){
     if(name == ws[names].v){
       if(phone == ws['C'+names.slice(1)].v){
         return {
-          name:ws['A'+names.slice(1)].v,
-          address:ws['B'+names.slice(1)].v,
+          name1:ws['A'+names.slice(1)].v,
+          name2:ws['B'+names.slice(1)].v,
           phone:ws['C'+names.slice(1)].v,
-          price:ws['D'+names.slice(1)].v,
-          count:ws['E'+names.slice(1)].v
+          address:ws['E'+names.slice(1)].v,
+          email:ws['G'+names.slice(1)].v,
+          price:ws['H'+names.slice(1)].v,
+          count:ws['I'+names.slice(1)].v
         };
       }
     }
@@ -38,7 +37,7 @@ app.get('/search',function (req,res) {
   res.render('search',{check : search(req.query.name,req.query.phone)})
 });
 //------
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 9000;
 app.listen(port,function () {
   console.log('Listening on '+ port);
 });
