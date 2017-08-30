7feb75f81b00c69727e798de9e1ff2346eba5989
chiol <jcy6677@gmail.com>
2017-08-30 14:03:54 +0900

    excel29

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ if(typeof require !== 'undefined') XLSX = require("xlsx");
 const wb = XLSX.readFile('list.xlsx');
 var ws = wb.Sheets.Sheet1;
 function search(name,phone) {
+  var result = [];
   for(names in ws){
-    if(name == ws[names].v){
-      if(phone == ws['I'+names.slice(1)].v){
-        return {
+    if(names[0] == 'H'){
+      if(name == ws[names].v){
+        if(phone == ws['I'+names.slice(1)].v){
+          console.log(names);
+          result.push({
           name1:ws['E'+names.slice(1)].v,
           name2:ws['H'+names.slice(1)].v,
           phone:ws['I'+names.slice(1)].v,
@@ -17,12 +20,15 @@ function search(name,phone) {
           price:ws['Q'+names.slice(1)].v,
           count:ws['R'+names.slice(1)].v,
           etc:ws['S'+names.slice(1)].v
-        };
+          });
+        }
       }
     }
   }
-  return false;
+  return [result,result.length];
 }
+
+
 //----
 app.use(express.static('public'));
 //template
@@ -35,7 +41,8 @@ app.get('/',function (req,res) {
   res.render('form');
 });
 app.get('/search',function (req,res) {
-  res.render('search',{check : search(req.query.name,req.query.phone)})
+  var result = search(req.query.name,req.query.phone);
+  res.render('search',{check : result[0], length :result[1] })
 });
 //------
 var port = process.env.PORT || 9000;
